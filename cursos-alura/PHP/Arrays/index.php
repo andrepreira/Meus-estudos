45b7ee9e4217b8aee08c458807f246dcd0572f77
André Pereira <andre.pereira.fisica@gmail.com>
2021-07-30 19:24:54 -0300

    atualizações - fazer curso 2 amanhã

---
+++ cursos-alura/PHP/Arrays/index.php
@@ -0,0 +1,13 @@
+<?php
+
+$notaPortugues = 9;
+$notaMatematica = 3;
+$notaGeofrafica = 10;
+$notaHistoria = 5;
+
+echo "A nota de Português é: $notaPortugues";
+echo "A nota de Português é: $notaMatematica";
+echo "A nota de Português é: $notaGeofraficanotaGeofrafica";
+echo "A nota de Português é: $notaHistoria";
+
+$media = ($notaPortugues+$notaMatematica)/4;
