7726a0cac0ffdfe97af89a5b1565426d9be7532f
André Pereira <andre.pereira.fisica@gmail.com>
2021-08-19 23:56:32 -0300

    esutodos python

--- cursos-alura/Python/jogos/forca.py
+++ cursos-alura/Python/jogos/forca.py
@@ -3,7 +3,38 @@ def jogar():
     print("Bem vindo ao jogo de Forca********!")
     print("**********************************")
 
+    palavra_secreta = "banana".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
+    letras_faltando = str(letras_acertadas.count('_'))
+    print('Ainda faltam acertar {} letras'.format(letras_faltando))
 
+    enforcou = False
+    acertou = False
+    erros = 0
+
+    print(letras_acertadas)
+
+    while(not enforcou and not acertou):
+        print("jogando...")
+        chute = input("Qual letra?")
+        chute = chute.strip().upper()
+
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
+
+    if(acertou):
+        print("Você ganhou!")
+    else:
+        print("Você perdeu!")
     print("Fim do jogo!")
 
 if(__name__ == "__main__"):
